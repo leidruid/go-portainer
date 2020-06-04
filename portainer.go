dd860fe9909dc0ec9328697bb454de8dfd4a8fce
Четвериков Евгений <leidruid@leidruid.ru>
2020-06-04 13:15:22 +0300

    init commit

---
+++ portainer.go
@@ -0,0 +1,144 @@
+package portainer
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func NewPortainer(c *Config) Portainer {
+	return Portainer{
+		Config: c,
+		Token:  "",
+		ApiURL: fmt.Sprintf("%s://%s:%s%s", c.Schema, c.Host, c.Port, c.URL),
+	}
+}
+
+func (p *Portainer) Auth() error {
+	authData := make(map[string]string)
+	authData["Username"] = p.Config.User
+	authData["Password"] = p.Config.Password
+	payload, err := json.Marshal(&authData)
+	res, err := http.Post(p.ApiURL+"/auth", "application/json", bytes.NewReader(payload))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		return errors.New("unauthorized")
+	}
+	jwtString, err := ioutil.ReadAll(res.Body)
+	_ = res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	jwtData := make(map[string]string)
+	_ = json.Unmarshal(jwtString, &jwtData)
+	p.Token = jwtData["jwt"]
+	return err
+}
+
+func (p *Portainer) ListEndpoints() ([]Endpoint, error) {
+	url := "/endpoints"
+	res, err := p.makeRequest("GET", url, nil, nil)
+	if err != nil {
+		log.Printf("http.Do() error: %v\n", err)
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	_ = res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var endpoints []Endpoint
+	err = json.Unmarshal(data, &endpoints)
+	if err != nil {
+		log.Printf("Endpoints unmarshaling error: %v\n", err)
+		return nil, err
+	}
+	return endpoints, err
+}
+
+func (p *Portainer) ListContainers(e int32) ([]Container, error) {
+	url := fmt.Sprintf("/endpoints/%d/docker/containers/json", e)
+	urlargs := make(map[string]string)
+	urlargs["all"] = "1"
+	res, err := p.makeRequest("GET", url, nil, urlargs)
+	if err != nil {
+		log.Printf("http.Do() error: %v\n", err)
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	_ = res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var containers []Container
+	err = json.Unmarshal(data, &containers)
+	return containers, nil
+}
+
+//noinspection GoNilness
+func (p *Portainer) StopContainer(e int32, id string) (int, error) {
+	url := fmt.Sprintf("/endpoints/%d/docker/containers/%s/stop", e, id)
+	res, err := p.makeRequest("POST", url, nil, nil)
+	if err != nil {
+		log.Printf("http.Do(%v) error: %v\n", res.Request.URL, err)
+		return 0, err
+	}
+	_ = res.Body.Close()
+	switch res.StatusCode {
+	case http.StatusNoContent:
+		return res.StatusCode, nil
+	case http.StatusInternalServerError:
+		return res.StatusCode, errors.New(fmt.Sprintf("InternalServerError: (%s)", url))
+	case http.StatusNotFound:
+		return res.StatusCode, errors.New(fmt.Sprintf("Not found: (%s)", url))
+	default:
+		return res.StatusCode, errors.New(fmt.Sprintf("UnhandledError %d: (%s)", res.StatusCode, url))
+	}
+}
+
+//noinspection GoNilness
+func (p *Portainer) StartContainer(e int32, id string) (int, error) {
+	url := fmt.Sprintf("/endpoints/%d/docker/containers/%s/start", e, id)
+	res, err := p.makeRequest("POST", url, nil, nil)
+	if err != nil {
+		log.Printf("http.Do(%v) error: %v\n", res.Request.URL, err)
+		return 0, err
+	}
+	_ = res.Body.Close()
+	switch res.StatusCode {
+	case http.StatusNoContent:
+		return res.StatusCode, nil
+	case http.StatusInternalServerError:
+		return res.StatusCode, errors.New(fmt.Sprintf("InternalServerError: (%s)", url))
+	case http.StatusNotFound:
+		return res.StatusCode, errors.New(fmt.Sprintf("Not found: (%s)", url))
+	case http.StatusNotModified:
+		return res.StatusCode, errors.New(fmt.Sprintf("Already started: (%s)", url))
+	default:
+		return res.StatusCode, errors.New(fmt.Sprintf("UnhandledError %d: (%s)", res.StatusCode, url))
+	}
+}
+
+func (p *Portainer) makeRequest(t string, url string, body io.Reader, args map[string]string) (*http.Response, error) {
+	urlargs := "?"
+	for k, v := range args {
+		urlargs += fmt.Sprintf("%s=%s", k, v)
+	}
+	if urlargs == "?" {
+		urlargs = ""
+	}
+	req, err := http.NewRequest(t, p.ApiURL+url+urlargs, body)
+	if err != nil {
+		log.Printf("http.NewRequest() error: %v\n", err)
+	}
+	req.Header.Add("Authorization", "Bearer "+p.Token)
+	c := &http.Client{}
+	return c.Do(req)
+}
---
+++ types.go
@@ -0,0 +1,74 @@
+package portainer
+
+type Config struct {
+	Host     string
+	Port     string
+	Schema   string
+	User     string
+	Password string
+	URL      string
+}
+
+type Portainer struct {
+	Config *Config
+	Token  string
+	ApiURL string
+}
+
+type Endpoint struct {
+	Id        int32    `json:"Id,omitempty"`
+	Name      string   `json:"Name,omitempty"`
+	URL       string   `json:"URL,omitempty"`
+	PublicURL string   `json:"PublicURL,omitempty"`
+	GroupID   int32    `json:"GroupID,omitempty"`
+	Tags      []string `json:"Tags"`
+}
+
+type Container struct {
+	ID      string   `json:"Id"`
+	Names   []string `json:"Names"`
+	Image   string   `json:"Image"`
+	ImageID string   `json:"ImageID"`
+	Command string   `json:"Command"`
+	Created int      `json:"Created"`
+	State   string   `json:"State"`
+	Status  string   `json:"Status"`
+	Ports   []struct {
+		PrivatePort int    `json:"PrivatePort"`
+		PublicPort  int    `json:"PublicPort"`
+		Type        string `json:"Type"`
+	} `json:"Ports"`
+	Labels     map[string]string `json:"Labels,omitempty"`
+	SizeRw     int               `json:"SizeRw"`
+	SizeRootFs int               `json:"SizeRootFs"`
+	HostConfig struct {
+		NetworkMode string `json:"NetworkMode"`
+	} `json:"HostConfig"`
+	NetworkSettings struct {
+		Networks struct {
+			Bridge struct {
+				IPAMConfig          interface{} `json:"IPAMConfig"`
+				Links               interface{} `json:"Links"`
+				Aliases             interface{} `json:"Aliases"`
+				NetworkID           string      `json:"NetworkID"`
+				EndpointID          string      `json:"EndpointID"`
+				Gateway             string      `json:"Gateway"`
+				IPAddress           string      `json:"IPAddress"`
+				IPPrefixLen         int         `json:"IPPrefixLen"`
+				IPv6Gateway         string      `json:"IPv6Gateway"`
+				GlobalIPv6Address   string      `json:"GlobalIPv6Address"`
+				GlobalIPv6PrefixLen int         `json:"GlobalIPv6PrefixLen"`
+				MacAddress          string      `json:"MacAddress"`
+			} `json:"bridge"`
+		} `json:"Networks"`
+	} `json:"NetworkSettings"`
+	Mounts []struct {
+		Name        string `json:"Name"`
+		Source      string `json:"Source"`
+		Destination string `json:"Destination"`
+		Driver      string `json:"Driver"`
+		Mode        string `json:"Mode"`
+		RW          bool   `json:"RW"`
+		Propagation string `json:"Propagation"`
+	} `json:"Mounts"`
+}
